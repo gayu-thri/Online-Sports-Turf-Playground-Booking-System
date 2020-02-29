4c1e9ddf13280fb7fa5066eb4f70b94a0e83b709
Gayathri Ethiraj <39553592+gayu-thri@users.noreply.github.com>
2020-02-29 16:49:03 +0530

    Minor changes updated

--- sports.py
+++ sports.py
@@ -92,3 +92,7 @@ def logout():
         global active
         active = None
         return home()
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='127.0.0.1', port=4000)
