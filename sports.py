025bc7d39c38ce90aac1c43dd8d872a0a1dad506
Gayathri Ethiraj <39553592+gayu-thri@users.noreply.github.com>
2020-02-29 17:48:11 +0530

    Added admin functionalities

--- sports.py
+++ sports.py
@@ -24,8 +24,11 @@ app = Flask(__name__)
 global active
 global email
 global visit
-'''
 global location
+
+location=['Chennai','Bangalore','Coimbatore']
+
+'''
 global status
 global courier
 
@@ -75,32 +78,73 @@ def do_admin_login():
             return redirect('/error')
         return render_template('home_manager.html',email=active)
 
-@app.route('/home', methods=["POST"])
-def home_page():
-        global visit
-        visit = visit + 1
-        if request.form['submit_button'] == 'View visits':
-            return render_template('visitors.html', v=visit)
-        if request.form['submit_button'] == 'Log out':
-            return logout()
-        if request.form['submit_button'] == 'Contact':
-            return render_template('contact.html')
+@app.route('/home_admin', methods=["POST"])
+def home_admin():
+    global visit
+    visit = visit + 1
+
+    global location
+    if request.form['submit_button'] == 'Add turf location':
+        return render_template('add_location.html',l = ",".join(location))
+
+    if request.form['submit_button'] == 'Allocate a manager':
+        return render_template('allocate_manager.html')
+    if request.form['submit_button'] == 'Add price list':
+        return render_template('add_price.html')
+    if request.form['submit_button'] == 'View booking':
+        return render_template('view_booking.html')
+
+    if request.form['submit_button'] == 'View visits':
+        return render_template('visitors.html', v=visit)
+    if request.form['submit_button'] == 'Log out':
+        return logout()
+    if request.form['submit_button'] == 'Contact':
+        return render_template('contact.html')
+
+@app.route('/add_location',methods=["POST"])
+def add_location():
+    global location
+    if request.form['submit_button'] == 'Add':
+        location.append(request.form['location'])
+        return "After addition, the available locations are: " + ",".join(location)
+
+@app.route('/home_manager', methods=["POST"])
+def home_manager():
+    global visit
+    visit = visit + 1
+    if request.form['submit_button'] == 'View visits':
+        return render_template('visitors.html', v=visit)
+    if request.form['submit_button'] == 'Log out':
+        return logout()
+    if request.form['submit_button'] == 'Contact':
+        return render_template('contact.html')
+
+@app.route('/home_user', methods=["POST"])
+def home_user():
+    global visit
+    visit = visit + 1
+    if request.form['submit_button'] == 'View visits':
+        return render_template('visitors.html', v=visit)
+    if request.form['submit_button'] == 'Log out':
+        return logout()
+    if request.form['submit_button'] == 'Contact':
+        return render_template('contact.html')
 
 @app.route('/contactexp', methods=["GET"])
 def contactexp():
-        return render_template('contactexp.html')
+    return render_template('contactexp.html')
 
 @app.route('/error')
 def err():
-        flash("Wrong password entered")
-        return home()
+    flash("Wrong password entered")
+    return home()
 
 @app.route("/logout")
 def logout():
-        session['logged_in'] = False
-        global active
-        active = None
-        return home()
+    session['logged_in'] = False
+    global active
+    active = None
+    return home()
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
